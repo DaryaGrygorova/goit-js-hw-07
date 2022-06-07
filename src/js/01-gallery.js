50b70eaa6f59e59570feea0187fa55551da5f0a0
DaryaGrygorova <grygorova.darya@gmail.com>
2022-06-07 21:17:33 +0300

    Готове домашнє завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,63 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// 1. Створення розмітки карток галереї
+const galleryCards = galleryItems
+  .map(({ preview, original, description }) => {
+    return ` <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+    </div>`;
+  })
+  .join(' ');
+
+// 2. Додавання розмітки в DOM, підключення слухача подій
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', galleryCards);
+galleryEl.addEventListener('click', onCardClick);
+
+// 3. Функція обробки кліку на картку галереї
+function onCardClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName === `IMG`) {
+    createModalImageViewer(event.target.dataset.source);
+  }
+}
+
+// 4. Створення розмітки та відкриття модального вікна перегляду зображення
+let modalImageViewer;
+
+function createModalImageViewer(url) {
+  modalImageViewer = basicLightbox.create(`<img src="${url}" width="800" height="600">`, {
+    onShow: addWindowEventListener,
+    onClose: removeWindowEventListener,
+  });
+  modalImageViewer.show();
+}
+
+// 5. Реалізація закриття модального вікна з клавіатури
+function addWindowEventListener() {
+  window.addEventListener('keydown', onEscapeKeyPress);
+  return true;
+}
+
+function onEscapeKeyPress(event) {
+  if (event.code === 'Escape') {
+    modalImageViewer.close();
+    return true;
+  }
+}
+
+function removeWindowEventListener() {
+  window.removeEventListener('keydown', onEscapeKeyPress);
+}
+
+console.log(galleryCards);
+
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// 1. Створення розмітки карток галереї
+const galleryCards = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  })
+  .join(' ');
+
+// 2. Додавання розмітки в DOM, підключення слухача подій
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', galleryCards);
+galleryEl.addEventListener('click', onCardClick);
+
+// 3. Функція обробки кліку на картку галереї
+function onCardClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName === `IMG`) {
+    createModalGallery();
+  }
+}
+
+// 4. Створення розмітки та відкриття модального вікна перегляду зображення
+function createModalGallery() {
+  const modalGallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  modalGallery.open();
+}
+
+console.log(galleryCards);
